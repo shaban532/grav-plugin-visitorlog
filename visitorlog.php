0cc220fbc522c9883615fa968d489141dd0ba6af
Sibiya <shaban532@gmail.com>
2024-05-15 11:43:32 +0330

    Update visitorlog.php

--- visitorlog.php
+++ visitorlog.php
@@ -21,14 +21,22 @@ class VisitorlogPlugin extends Plugin
     {
 	date_default_timezone_set("Asia/Tehran");
 	$ip = "IP: "  . $_SERVER['REMOTE_ADDR'];
-	$time= "	Time: " . date("Y/m/d h:i:sa");
+	$time= "Time: " . date("Y/m/d h:i:sa");
 	$day=  date("Ymd");
-	$address = "	" . $_SERVER['REQUEST_URI'];
+	$address =  $_SERVER['REQUEST_URI'];
 	$log = 'log/' . $day;
-	$old = file_get_contents($log);
-	$file = fopen($log, 'w+');
-	fwrite($file,$ip . $time . $address . "\n" . $old);
-	fclose($file);
+	$referer ="\nSource: ";
+	if ( array_key_exists('HTTP_REFERER', $_SERVER )){
+		$referer = $referer . $_SERVER['HTTP_REFERER'];
+	}
+		
+	$old = '';
+	if ( file_exists($log)){
+		$old = file_get_contents($log);
+	}		
+		$file = fopen($log, 'w+');
+		fwrite($file,$ip ."\n" . $time . "\n" . $address . $referer . "\n\n" . $old);
+		fclose($file);
     }
 
 }
